b9f5983fbc2e9a433619731f38e9e6947a467acf
yusanshi <yulei2018@gmail.com>
2019-05-16 17:19:38 +0800

    finish phase 1 subphase A

--- cs140e/os/kernel/src/lang_items.rs
+++ cs140e/os/kernel/src/lang_items.rs
@@ -1,9 +1,28 @@
+use console::kprintln;
+
 #[no_mangle]
 #[cfg(not(test))]
 #[lang = "panic_fmt"]
 pub extern fn panic_fmt(fmt: ::std::fmt::Arguments, file: &'static str, line: u32, col: u32) -> ! {
     // FIXME: Print `fmt`, `file`, and `line` to the console.
 
+    kprintln!(r#"            (              	"#);
+    kprintln!(r#"       (      )     )			"#);
+    kprintln!(r#"         )   (    (			"#);
+    kprintln!(r#"        (          `			"#);
+    kprintln!(r#"    .-""^"""^""^"""^""-.		"#);
+    kprintln!(r#"  (//\\//\\//\\//\\//\\//)		"#);
+    kprintln!(r#"   ~\^^^^^^^^^^^^^^^^^^/~		"#);
+    kprintln!(r#"     `================`		"#);
+    kprintln!("");
+    kprintln!(r#"    The pi is overdone.		"#);
+    kprintln!("");
+    kprintln!(r#"---------- PANIC ----------	"#);
+    kprintln!("");
+    kprintln!("FILE: {}",file);
+    kprintln!("LINE: {}",line);
+    kprintln!("COL: {}",col);
+    kprintln!("{}", fmt);
     loop { unsafe { asm!("wfe") } }
 }
 
