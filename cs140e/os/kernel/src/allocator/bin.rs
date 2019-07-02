490ef2e9f465015ee9e4f4b8704f4758583ee016
zhengfengL <1274540173@qq.com>
2019-07-02 07:00:11 -0700

    debug

--- cs140e/os/kernel/src/allocator/bin.rs
+++ cs140e/os/kernel/src/allocator/bin.rs
@@ -33,7 +33,7 @@ impl Allocator {
 		//construct the free-space list.
 		while current + MIN_SIZE <= end {
 			//decide the size of chunk.
-			let size = min((end - current).next_power_of_two() >> 2, 
+			let size = min((end - current).next_power_of_two() >> 1, 
 							1 << current.trailing_zeros());
 			
 			//push the chunk to list and renew the members
@@ -81,21 +81,23 @@ impl Allocator {
 		//decide allocating size
         let size = max(layout.size().next_power_of_two(),
 						max(layout.align(), MIN_SIZE));
-		let index = size.trailing_zeros();
+		let index = size.trailing_zeros() as usize;
 		
 		//allocate chunk and adjust the list
-		for i in index..self.list.len() as u32 {
-			if self.list[i as usize].is_empty() { continue }
-			let start = self.list[i as usize].pop().unwrap();
+		for i in index..self.list.len() {
+			if self.list[i].is_empty() { continue }
 			//handle the fragments， using buddy system like linux
-			for j in index..i {
+			for j in (index + 1..i + 1).rev() {
+				let block = self.list[j].pop().expect("bigger block should have free space");
 				unsafe {
-					let buddy = start.add(1 << j) as *mut usize;
-					self.list[j as usize].push(buddy);
+					self.list[j-1].push((block as usize + (1 << (j - 1))) as *mut usize);
+                	self.list[j-1].push(block);
 				}
 			}
 			//return the start address
-			return Ok(start as *mut u8);
+			let start = Ok(self.list[index].pop().expect("current block should have free space now") as *mut u8);
+			self.allocated += size;
+			return start;
 		}
 		
 		//cannot find proper chunk, the memory is exhausted
@@ -119,32 +121,41 @@ impl Allocator {
 		//decide deallocating size
         let size = max(layout.size().next_power_of_two(),
 						max(layout.align(), MIN_SIZE));
-		let index = size.trailing_zeros(); 
+		let index = size.trailing_zeros() as usize; 
 
 		//deallocate the chunk and trying to merge chunks according to buddy system strategy
-		let mut addr = ptr as usize;
-		for i in index..self.list.len() as u32 {
-			let mut flag = false;
-			let buddy = addr ^ (1 << i);
+		unsafe{
+			self.list[index].push(ptr as *mut usize);
+			let mut addr = ptr as usize;
+			let mut current = index;
+			loop {
+				let mut flag = false;
+				let buddy = addr ^ (1 << current);
+				//try to find the buddy chunk
+				for node in self.list[current].iter_mut() {
+					if node.value() as usize == buddy {
+						//find the buddy chunk, set flag to true
+						node.pop();
+						flag = true;
+						break;
+					}
+				}
 			
-			//try to find the buddy chunk
-			for node in self.list[i as usize].iter_mut() {
-				if node.value() as usize == addr {
-					//find the buddy chunk, go to the next list
-					node.pop();
-					flag = true;
-					addr = min(addr, buddy);
+				//according to the flag 
+				if flag {
+					//find the buddy, go to the next list
+					self.list[current].pop();
+                	addr = min(addr, buddy);
+					current += 1;
+                	self.list[current].push(addr as *mut usize);
+				} else {
+					//cannot find the buddy, break
 					break;
 				}
 			}
-			
-			//cannot find the buddy, just push that chunk
-			if !flag {
-				unsafe {
-					self.list[i as usize].push(addr as *mut usize);
-				}
-			}
 		}
+
+		self.allocated -= size;
     }
 }
 //
@@ -158,3 +169,153 @@ impl fmt::Debug for Allocator {
 			.finish()
     }
 }
+
+/*
+const SIZEOF_USIZE: usize = size_of::<usize>();
+
+pub fn prev_power_of_two(num: usize) -> usize {
+    1 << (8 * (size_of::<usize>()) - num.leading_zeros() as usize - 1)
+}
+
+/// A simple allocator that allocates based on size classes.
+pub struct Allocator {
+    // FIXME: Add the necessary fields.
+    // minimum=usize=8B, 2^3B
+    free_list: [LinkedList; 32],
+    allocated: usize,
+    total: usize,
+}
+
+impl Allocator {
+    /// Creates a new bin allocator that will allocate memory from the region
+    /// starting at address `start` and ending at address `end`.
+    pub fn new(start: usize, end: usize) -> Allocator {
+        let mut free_list = [LinkedList::new(); 32];
+        let mut current_start = start;
+
+        let mut total = 0;
+        while current_start + SIZEOF_USIZE <= end {
+            let lowbit = current_start & (!current_start + 1);
+            let size = min(lowbit, prev_power_of_two(end - current_start));
+            total += size;
+            unsafe {
+                free_list[size.trailing_zeros() as usize].push(current_start as *mut usize);
+            }
+            current_start += size;
+        }
+
+        Allocator {
+            free_list: free_list,
+            allocated: 0,
+            total,
+        }
+    }
+
+    /// Allocates memory. Returns a pointer meeting the size and alignment
+    /// properties of `layout.size()` and `layout.align()`.
+    ///
+    /// If this method returns an `Ok(addr)`, `addr` will be non-null address
+    /// pointing to a block of storage suitable for holding an instance of
+    /// `layout`. In particular, the block will be at least `layout.size()`
+    /// bytes large and will be aligned to `layout.align()`. The returned block
+    /// of storage may or may not have its contents initialized or zeroed.
+    ///
+    /// # Safety
+    ///
+    /// The _caller_ must ensure that `layout.size() > 0` and that
+    /// `layout.align()` is a power of two. Parameters not meeting these
+    /// conditions may result in undefined behavior.
+    ///
+    /// # Errors
+    ///
+    /// Returning `Err` indicates that either memory is exhausted
+    /// (`AllocError::Exhausted`) or `layout` does not meet this allocator's
+    /// size or alignment constraints (`AllocError::Unsupported`).
+    pub fn alloc(&mut self, layout: Layout) -> Result<*mut u8, AllocErr> {
+        let size = max(
+            layout.size().next_power_of_two(),
+            max(layout.align(), SIZEOF_USIZE),
+        );
+        let class = size.trailing_zeros() as usize;
+        for i in class..self.free_list.len() {
+            if !self.free_list[i].is_empty() {
+                for j in (class + 1..i + 1).rev() {
+                    let block = self.free_list[j]
+                        .pop()
+                        .expect("bigger block should have free space");
+                    unsafe {
+                        self.free_list[j - 1].push((block as usize + (1 << (j - 1))) as *mut usize);
+                        self.free_list[j - 1].push(block);
+                    }
+                }
+
+                let result = Ok(self.free_list[class]
+                    .pop()
+                    .expect("current block should have free space now")
+                    as *mut u8);
+                self.allocated += size;
+                return result;
+            }
+        }
+        Err(AllocErr::Exhausted{ request: layout})
+    }
+
+    /// Deallocates the memory referenced by `ptr`.
+    ///
+    /// # Safety
+    ///
+    /// The _caller_ must ensure the following:
+    ///
+    ///   * `ptr` must denote a block of memory currently allocated via this
+    ///     allocator
+    ///   * `layout` must properly represent the original layout used in the
+    ///     allocation call that returned `ptr`
+    ///
+    /// Parameters not meeting these conditions may result in undefined
+    /// behavior.
+    pub fn dealloc(&mut self, ptr: *mut u8, layout: Layout) {
+        let size = max(
+            layout.size().next_power_of_two(),
+            max(layout.align(), SIZEOF_USIZE),
+        );
+        let class = size.trailing_zeros() as usize;
+
+        unsafe {
+            self.free_list[class].push(ptr as *mut usize);
+            let mut current_ptr = ptr as usize;
+            let mut current_class = class;
+            loop {
+                let buddy = current_ptr ^ (1 << current_class);
+                let mut flag = false;
+                for block in self.free_list[current_class].iter_mut() {
+                    if block.value() as usize == buddy {
+                        block.pop();
+                        flag = true;
+                        break;
+                    }
+                }
+                if flag {
+                    self.free_list[current_class].pop();
+                    current_ptr = min(current_ptr, buddy);
+                    current_class += 1;
+                    self.free_list[current_class].push(current_ptr as *mut usize);
+                } else {
+                    break;
+                }
+            }
+        }
+
+        self.allocated -= size;
+    }
+}
+
+// FIXME: Implement `Debug` for `Allocator`.
+impl fmt::Debug for Allocator {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        fmt.debug_struct("BinAllocator")
+            .field("allocated", &self.allocated)
+            .field("total", &self.total)
+            .finish()
+    }
+}
+*/
